3a44eee6f13ac3e0f3cbff19b7b0ab06314352f7
ArunkumarAK123 <arunmurugan511@gmail.com>
2025-09-21 18:58:04 +0530

    projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Home from './components/Home';
-// import Projects from './components/Projects';
+import Projects from './components/Projects';
 // import Contact from './components/Contact';
   
 function App() {
   return (
     <>
       <Home />
-      {/* <Projects /> */}
+       <Projects /> 
       {/* <Contact /> */}
     </>
   );
